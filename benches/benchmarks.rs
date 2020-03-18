0fa5786447dcf9fdb9361ab88c2e193a0ce5ca57
Ryan Lopopolo <rjl@hyperbo.la>
2020-03-17 21:25:02 -0700

    Update bench

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -3,17 +3,17 @@ extern crate test;
 
 mod mt19937 {
     use rand_core::RngCore;
-    use rand_mt::MT19937;
+    use rand_mt::Mt;
 
     #[bench]
     fn benchmark_seeding(b: &mut ::test::Bencher) {
-        b.iter(MT19937::new_unseeded);
+        b.iter(Mt::new_unseeded);
     }
 
     #[bench]
     fn benchmark_fill_next_state(b: &mut ::test::Bencher) {
         b.iter(|| {
-            let mut mt = MT19937::new_unseeded();
+            let mut mt = Mt::new_unseeded();
             // Note that the first call to next_u32() triggers a call
             // to the fill_next_state() method, which is really what I
             // want to benchmark here.
@@ -24,17 +24,17 @@ mod mt19937 {
 
 mod mt19937_64 {
     use rand_core::RngCore;
-    use rand_mt::MT19937_64;
+    use rand_mt::Mt64;
 
     #[bench]
     fn benchmark_seeding(b: &mut ::test::Bencher) {
-        b.iter(MT19937_64::new_unseeded);
+        b.iter(Mt64::new_unseeded);
     }
 
     #[bench]
     fn benchmark_fill_next_state(b: &mut ::test::Bencher) {
         b.iter(|| {
-            let mut mt = MT19937_64::new_unseeded();
+            let mut mt = Mt64::new_unseeded();
             // Note that the first call to next_u32() triggers a call
             // to the fill_next_state() method, which is really what I
             // want to benchmark here.
